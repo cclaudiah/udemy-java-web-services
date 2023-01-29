373aa564d4f94d9271d655afc081de53fdf86d06
cclaudiah <claudia.carrascoh@gmail.com>
2023-01-29 20:51:33 -0300

    S07-049: Se crea proyecto jaxb

---
+++ jaxbxjc/src/main/java/cl/claudia/jaxb/jaxbxjc/App.java
@@ -0,0 +1,13 @@
+package cl.claudia.jaxb.jaxbxjc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
