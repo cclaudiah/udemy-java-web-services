1e1abc09909fff7ec42d76f8bd99eb1e0c183c8c
cclaudiah <claudia.carrascoh@gmail.com>
2023-02-01 20:56:13 -0300

    S07-068: Se agrega log al ejecutar servicio.

--- hellowebservice/src/main/java/cl/claudia/ws/soap/HelloWs.java
+++ hellowebservice/src/main/java/cl/claudia/ws/soap/HelloWs.java
@@ -1,10 +1,13 @@
 package cl.claudia.ws.soap;
 
 
+import org.apache.cxf.feature.Features;
+
 import jakarta.jws.WebMethod;
 import jakarta.jws.WebService;
 
 @WebService
+@Features(features="org.apache.cxf.feature.LoggingFeature")
 public class HelloWs {
 	
 	@WebMethod
